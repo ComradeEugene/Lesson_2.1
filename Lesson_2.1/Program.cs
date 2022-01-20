1dc77b1e503f6984e3a6ce675b002610f5c773bb
Eugene <comradeeugene@gmail.com>
2022-01-20 21:49:31 +0300

    End

--- Lesson_2.1/Program.cs
+++ Lesson_2.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lesson_2
 {
@@ -6,7 +7,29 @@ namespace Lesson_2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string FullName = "Ленин Вланимир Ильич";
+            string Mail = "RevolutionBoss@mushroom.ru";
+            int Age = 35;
+            double Programming = 99.9;
+            double Mathematics = 100.0;
+            double Physics = 100.0;
+            double Sum = Physics + Mathematics + Programming;
+            double Average = Sum / 3;
+            string Form = "ФИО: {0}\nВозраст: {1}\nEmail: {2}\nБаллы по " +
+                          "программированию: {3}\nБаллы по математике: {4}" +
+                          "\nБаллы по физике: {5}";
+
+            WriteLine(Form,
+                      FullName,
+                      Age,
+                      Mail,
+                      Programming,
+                      Mathematics,
+                      Physics);
+            ReadKey();
+            WriteLine($"Сумма баллов: {Sum}\nСреднее" +
+                      $"арифметическое: {Average}");
+            ReadKey();
         }
     }
 }
